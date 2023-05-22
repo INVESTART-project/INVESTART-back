400d7695ba1850561251d657e3aae45f03ad84f2
Shatsckij <shatsckij.i@yandex.ru>
2023-05-22 19:54:02 +0300

    blevota

---
+++ src/main/java/ru/project/Investart/entity/Car.java
@@ -0,0 +1,13 @@
+package ru.project.Investart.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PUBLIC, force = true)
+//@RequiredArgsConstructor
+public class Car {
+
+    String name;
+}
